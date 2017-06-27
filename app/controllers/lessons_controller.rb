28d60421a3db03e45e1d97d456b70b0b94a2ea00
Ali Thompson <alilynne.thompson@gmail.com>
2017-06-27 19:38:30 +0000

    permissions challenge

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_lesson.section.course.user != current_user && !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Must be enrolled in course to view lessons'
+    end
+  end
 
   helper_method :current_lesson
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+
 end
